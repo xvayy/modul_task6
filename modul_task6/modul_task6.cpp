f63e3b3e49c369b29d8237bfac7da19f673edce0
Yula <ivanykjula@gmail.com>
2024-03-19 10:50:29 +0200

    Add project files.

---
+++ modul_task6/modul_task6.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+// Хвостова рекурсивна функція для обчислення суми перших n натуральних чисел
+int tailRecursiveSum(int n, int sum = 0) {
+    if (n == 0) {
+        return sum;
+    }
+    return tailRecursiveSum(n - 1, sum + n);
+}
+
+int main() {
+    int n;
+    cout << "Enter n: ";
+    cin >> n;
+
+    int sum = tailRecursiveSum(n);
+    cout << "The sum " << n << " of the first natural numbers: " << sum << endl;
+
+    return 0;
+}
